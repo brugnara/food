f8a9e0edd3a8b1e9dfa0fc95155b0e62db2ba9d1
Daniele Brugnara <daniele@brugnara.me>
2019-10-17 18:07:47 +0200

    initial

--- js/index.js
+++ js/index.js
@@ -478,7 +478,7 @@
   headingNodes.forEach(function(node){
     link = createEl('a');
     icon = createEl('img');
-    icon.src = 'http://example.org/images/icons/link.svg';
+    icon.src = 'https://food.brugnara.me/images/icons/link.svg';
     link.className = 'link';
     link.appendChild(icon);
     id = node.getAttribute('id');
